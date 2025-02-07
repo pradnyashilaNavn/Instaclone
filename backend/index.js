56be840465a9f5089c7222210560b58a0bb1f8bd
unknown <pradnyajadhav995@gmail.com>
2025-02-07 16:50:39 +0530

    ready for deployment

--- backend/index.js
+++ backend/index.js
@@ -7,12 +7,14 @@ import userRoute from "./routes/user.route.js";
 import postRoute from "./routes/post.route.js";
 import messageRoute from "./routes/message.route.js";
 import { app, server } from "./socket/socket.js";
+import path from "path";
 
 dotenv.config({});
 
 //const app = express();
 const PORT = process.env.PORT || 3000;
-
+const __dirname = path.resolve();
+console.log(__dirname);
 
 app.get("/", (_, res) => {
     return res.status(200).json({
@@ -34,6 +36,11 @@ app.use(cors(corsOptions));
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/post", postRoute);
 app.use("/api/v1/message", messageRoute);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req,res)=>{
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+})
 
 server.listen(PORT, () => {
     connectDB();
